c62977c668749588724e148ffb5a5e531012538c
alexandra-carneiro <129199596+alexandra-carneiro@users.noreply.github.com>
2023-11-14 20:19:12 +0000

    Add files via upload

---
+++ src/src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles/normalize.css";
+import "./styles/fontawesome.min.css";
+
+// import Home from "./components/Home/Home";
+import Feed from "./components/Feed/Feed.jsx";
+
+function App() {
+  return <Feed />;
+}
+
+export default App;
